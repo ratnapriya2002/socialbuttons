c6def9e7dedc44c659a37fa15a468caa0111b5b1
ratnapriya2002 <chamalapriya@gmail.com>
2023-06-01 16:17:08 +0530

    Socialbuttons

---
+++ src/components/socialbuttons/index.js
@@ -0,0 +1,15 @@
+import React  from "react"; 
+import "./index.css"
+const Socialbuttons = () =>{
+    return(
+        <div className="bg-container">
+        <h1 className="heading">Social Buttons</h1>
+        <div className="buttons-container">
+           <button className="button1">Like</button> 
+           <button className="button2">submit</button> 
+           <button className="button3">Share</button> 
+        </div>
+        </div>
+    )
+}
+export default Socialbuttons
